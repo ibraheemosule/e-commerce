bd04155d90facaa40f7dadc352a7f6ad32a92460
Ibrahim Sule <ibraheemsulay@gmail.com>
2023-01-31 16:44:58 +0100

    added select field component

---
+++ src/components/others/select-field/SelectField.tsx
@@ -0,0 +1,37 @@
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Box from "@mui/material/Box";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { memo, useState } from "react";
+
+const SelectField = () => {
+  const [age, setAge] = useState("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <div>
+      <Box sx={{ minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-standard-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-standard-label"
+          id="demo-simple-select-standard"
+          value={age}
+          onChange={handleChange}
+          label="Age"
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </Box>
+    </div>
+  );
+};
+
+export default memo(SelectField);
