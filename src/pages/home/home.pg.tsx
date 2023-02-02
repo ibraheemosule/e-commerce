9a58dd2d2f054a2ef2f44120188100cf2b963449
Ibrahim Sule <ibraheemsulay@gmail.com>
2023-02-02 09:44:11 +0100

    fixed deploy path error

--- src/pages/home/home.pg.tsx
+++ src/pages/home/home.pg.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Home from "../../components/__pages/home/Home";
+import Home from "../../components/__pages/homee/Home";
 
 export default function HomePage() {
   return (
