9a04cbc9e467500d49f4433b64fa9444c3e70c71
Ibrahim Sule <ibraheemsulay@gmail.com>
2023-03-24 12:27:51 +0100

    changed product card height

--- src/components/others/product-card/u_productCard.tsx
+++ src/components/others/product-card/u_productCard.tsx
@@ -16,7 +16,7 @@ export const ImgBtn = styled(Box)(({ theme }) => ({
   },
   [theme.breakpoints.down("sm")]: {
     width: "100% !important",
-    height: 300,
+    height: 350,
   },
   borderColor: theme.palette.secondary.main,
 
